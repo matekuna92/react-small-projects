a39e06cab75c6314a86fd2381a3a1876d6b6259e
MATEKUNA <kuna.mate@parlament.hu>
2023-06-12 16:09:15 +0200

    FontAwesomeIcon package

--- 1-todo/src/components/TodoItem.js
+++ 1-todo/src/components/TodoItem.js
@@ -2,14 +2,28 @@ import React from 'react';
 
 import Card from "../ui/Card";
 import './TodoItem.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const TodoItem = (props) => {
+    const todoEditHandler = () => {
+        console.log('edit');
+    }
+
+    const todoDeleteHandler = (id) => {
+        console.log('delete');
+    }
+
     return (
         <Card>
             <li className='todo-item'>
                 <h2>{props.title}</h2>
                 <p>{props.desc}</p>
                 <img src={props.image} alt={props.title} />
+                <div className='todo-item-actions'>
+                    <FontAwesomeIcon icon={faEdit} onClick={todoEditHandler} style={{ color: 'blue', cursor: 'pointer' }} />
+                    <FontAwesomeIcon icon={faTrash} onClick={todoDeleteHandler} style={{ color: 'red', cursor: 'pointer' }} />
+                </div>
             </li>
         </Card>
     );
